40b4e8fec0cdea9334f318e0a909d0fc6c5f49c5
Nicholas Marr (1000015700) <MARRNJ2@student.op.ac.nz>
2019-04-22 13:53:36 +1200

    Added Move, Draw, Bounce methods to ball. Added basic information like ball size velocity and color

--- Pong/Pong/Ball.cs
+++ Pong/Pong/Ball.cs
@@ -1,14 +1,40 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Windows.Forms;
+
 
 namespace Pong
 {
     class Ball
     {
-        public Ball ()
+        private const int SIZE = 30;
+        private const int CHANGEDIRECTION = -1;
+
+
+        private Graphics graphics;
+        private List<Point> points;
+        private Brush brush;
+        private Point velocity;
+
+        public Ball (Graphics graphics, Size ClientSize)
+        {
+            this.graphics = graphics;
+        }
+        public void Draw()
+        {
+
+        }
+        public void Move()
+        {
+
+        }
+        public void Bounce()
         {
 
         }
--- Pong/Pong/World.cs
+++ Pong/Pong/World.cs
@@ -1,16 +1,22 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Windows.Forms;
+
 
 namespace Pong
 {
     class World
     {
-        public World ()
+        private Ball ball;
+        public World (Graphics graphics, Size ClientSize)
         {
-
+            ball = new Ball(graphics, ClientSize);
         }
     }
 }
